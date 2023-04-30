681e482257af5db7ecf4b2447376b4292f98323c
naya1503 <118755946+naya1503@users.noreply.github.com>
2023-04-30 09:56:21 +0700

    update initial.py

--- assistant/initial.py
+++ assistant/initial.py
@@ -10,35 +10,20 @@ import re
 from . import *
 
 STRINGS = {
-    1: """ **Thanks for Deploying Ayra Userbot!**
+    1: """ **Thanks for Deploying Aurora Userbot!**
 
 • Here, are the Some Basic stuff from, where you can Know, about its Usage.""",
-    2: """🎉** About Ayra**
+    2: """🎉** About Aurora-Userbot**
 
  Ayra is Pluggable and powerful Telethon and Pyrogram Userbot, made in Python from Scratch. It is Aimed to Increase Security along with Addition of Other Useful Features.
 
-❣ Kang by **@Rizzvbss**""",
-    3: """**💡• FAQs •**
-
--> [Username Tracker](https://t.me/UltroidUpdates/24)
--> [Keeping Custom Addons Repo](https://t.me/UltroidUpdates/28)
--> [Disabling Deploy message](https://t.me/UltroidUpdates/27)
--> [Setting up TimeZone](https://t.me/UltroidUpdates/22)
--> [About Inline PmPermit](https://t.me/UltroidUpdates/21)
--> [About Dual Mode](https://t.me/UltroidUpdates/18)
--> [Custom Thumbnail](https://t.me/UltroidUpdates/13)
--> [About FullSudo](https://t.me/UltroidUpdates/11)
--> [Setting Up PmBot](https://t.me/UltroidUpdates/2)
--> [Also Check](https://t.me/UltroidUpdates/14)
-
-**• To Know About Updates**
-  - Join @kynansupport.""",
-    4: f"""• `To Know All Available Commands`
+❣ For Aurora by **@KynanSupport**""",
+    3: f"""• `To Know All Available Commands`
 
   - `{HNDLR}help`
   - `{HNDLR}cmds`""",
-    5: """• **For Any Other Query or Suggestion**
-  - Move to **@kynansupport**.
+    4: """• **For Any Other Query or Suggestion**
+  - Go to **@KynanSupport**.
 
 • Thanks for Reaching till END.""",
 }
@@ -47,9 +32,9 @@ STRINGS = {
 @callback(re.compile("initft_(\\d+)"))
 async def init_depl(e):
     CURRENT = int(e.data_match.group(1))
-    if CURRENT == 5:
+    if CURRENT == 4:
         return await e.edit(
-            STRINGS[5],
+            STRINGS[4],
             buttons=Button.inline("Kembali", "initbk_4"),
             link_preview=False,
         )
